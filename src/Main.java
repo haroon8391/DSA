476584f81b7de1177f647cff95e32a1c5b86abd6
Muhammad Haroon Ali <haroon8391@gmail.com>
2024-05-22 21:57:35 +0500

    update changes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        LinkedList myList = new LinkedList(4);
+
+        myList.append(6);
+        myList.append(10);
+        myList.printList();
+
+        myList.prepend(2);
+        myList.printList();
+        myList.removeFirst();
+        System.out.println(myList.get(2).value);
     }
 }
